f45d3fe8e3bd86af1a70b7682da437cd3f184172
mszal <m.szalasz@gmail.com>
2024-12-02 16:04:26 +0100

    merge

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="sticky top-0  z-[20] mx-auto w-full items-center justify-between p-8 border-gay">
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;

61245fbef309f38dece0eeaa5a8b5c5d7665e2dc
mszal <m.szalasz@gmail.com>
2024-12-02 18:23:04 +0100

    data transfer commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-export default function Home() {
-  return <div className="h-full">
+import HomePage from "../components/HomePage";
 
+export default function Home() {
+  return <div>
+    <HomePage />
   </div>;
 }
---
+++ components/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+  <div className='flex flex-col'>
+    <div className='text-4xl'> InkCheck - porównaj ceny w jednym miejscu.</div>
+    <div>Status raportu: </div>
+  </div>
+  )
+}
+
+export default HomePage

a9d4ff5793a2b51b29c2a57e88dc4bc4e8ee08f9
mszal <m.szalasz@gmail.com>
2024-12-23 23:33:05 +0100

    something

--- components/raport/RaportPage.tsx
+++ components/raport/RaportPage.tsx
@@ -1,17 +1,49 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IProductPrices } from "../../types";
 import ProductPreview from "./ProductPreview";
 import fetchProductPrices from "services/priceService";
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
+
+interface ISearchOptions {
+  preciseName: boolean;
+  onlyAvailable: boolean;
+}
 
 const RaportPage = () => {
   const [product, setProduct] = useState<IProductPrices | null>(null);
   const [productId, setProductId] = useState<string>("");
   const [error, setError] = useState<string | null>("");
+  const [optionsOpen, setOptionsOpen] = useState<boolean>(false);
+
+  const getInitialSearchOptions = (): ISearchOptions => {
+    if (typeof window !== "undefined") {
+      const storedOptions = localStorage.getItem("searchOptions");
+      return storedOptions
+        ? JSON.parse(storedOptions)
+        : { preciseName: true, onlyAvailable: false };
+    }
+    return { preciseName: true, onlyAvailable: false };
+
+    const [searchOptions, setSearchOptions] = useState<ISearchOptions>(
+      getInitialSearchOptions,
+    );
+  };
+
+  useEffect(() => {
+    localStorage.setItem("searchOptions", JSON.stringify(searchOptions));
+  }, [searchOptions]);
+
+  const updateSearchOptions = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+    setSearchOptions((prev) => ({
+      ...prev,
+      [name]: checked,
+    }));
+  };
+
   const updateProduct = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(`Product ID: ${productId}`); // Added log
-    console.log(process.env.REACT_APP_API_ACCESS_KEY); // Added log
     if (productId === "") {
       setError("Musisz podać unikalny identyfikator produktu.");
       return;
@@ -57,6 +89,60 @@ const RaportPage = () => {
             Szukaj
           </button>
         </div>
+        {!optionsOpen ? (
+          <button
+            className="flex w-full items-center justify-center border border-gray-800 bg-black p-2 text-xl text-white duration-150 ease-in hover:bg-gray-800"
+            onClick={() => setOptionsOpen(!optionsOpen)}
+          >
+            Opcje wyszukiwania{" "}
+            <span>
+              <GoChevronDown />
+            </span>
+          </button>
+        ) : (
+          <div className="w-full border border-gray-800 bg-black">
+            <div className="w-full text-center text-xl text-white duration-150 ease-in hover:bg-gray-800">
+              <button
+                className="flex w-full items-center justify-center py-2"
+                onClick={() => setOptionsOpen(!optionsOpen)}
+              >
+                Zamknij
+                <span className="ml-2">
+                  <GoChevronUp />
+                </span>
+              </button>
+            </div>
+            <div className="items center flex flex-col gap-2 p-1">
+              <div className="flex items-baseline">
+                <input
+                  type="checkbox"
+                  id="preciseName"
+                  name="preciseName"
+                  value={searchOptions.preciseName}
+                  onChange={updateSearchOptions}
+                  className="cursor-pointer"
+                />
+                <label className="ml-1 cursor-pointer" htmlFor="preciseName">
+                  Dokładne wyszukiwanie nazwy
+                </label>
+              </div>
+              <div className="flex items-baseline">
+                <input
+                  type="checkbox"
+                  id="onlyAvailable"
+                  name="onlyAvailable"
+                  value={searchOptions.onlyAvailable}
+                  onChange={updateSearchOptions}
+                  className="cursor-pointer"
+                />
+                <label className="ml-1 cursor-pointer" htmlFor="onlyAvailable">
+                  Tylko dostępne
+                </label>
+              </div>
+            </div>
+          </div>
+        )}
+
         {error && <div className="text-red-500">{error}</div>}
       </form>
 
